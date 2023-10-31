f322d8450af5cc3c8c162f4d3cbe782f13ae5a34
Andrew Brooks <andrewgraemebrooks@gmail.com>
2023-10-31 21:18:08 +0000

    React hook form react native example

--- src/auth/LoginScreen.jsx
+++ src/auth/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm, Controller } from "react-hook-form"
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function LoginScreen() {
 const {
@@ -13,7 +14,52 @@ const {
     },
     })
     const onSubmit = (data) => console.log(data)
-  return (
-    <div>LoginScreen</div>
-  )
+
+    return (
+      <View style={styles.container}>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="First name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="firstName"
+        />
+        {errors.firstName && <Text>This is required.</Text>}
+  
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 100,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Last name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="lastName"
+        />
+  
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
+    )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
